67145b913b24948b566600ecdc72a14351501a64
evanhr18 <evanhr18@wfu.edu>
2019-10-03 14:45:46 -0400

    Class Exercise

--- main.cpp
+++ main.cpp
@@ -12,6 +12,18 @@ public:
     double getSalary() {return salary;}
     void setSalary(double salary);
 /* 1) create a getter and setter for each variable in the class*/
+    string getName() {return name;}
+    void setName(string name);
+    string getID() {return id;}
+    void setID(string id);
+    string getAddress() {return address;}
+    void setAddress(string address);
+    string getDoB() {return DoB;}
+    void setDoB(string DoB);
+    string getDegree() {return degree;}
+    void setDegree(string degree);
+    double getSSN() {return SSN;}
+    void setSSN(double SSN);
 
 
 private:
@@ -23,12 +35,45 @@ void Faculty::setSalary(double s){
     salary=s;
 }
 
+void Faculty::setName(string n) {
+    name = n;
+}
+
+void Faculty::setID(string i) {
+    id = i;
+}
+
+void Faculty::setAddress(string a) {address = a;}
+void Faculty::setDoB(string d) {DoB = d;}
+void Faculty::setDegree(string deg) {degree = deg;}
+void Faculty::setSSN(double ss) {
+    SSN = ss;
+}
+
 /* 2) implement each setter and getter */
 
 int main() {
     Faculty prof;
+    string temp;
+    double x;
 
 /* 3) use the implemented methods to fill prof' info and print them out */
+    cin >> temp;
+    prof.setAddress(temp);
+    cin >> temp;
+    prof.setDegree(temp);
+    cin >> temp;
+    prof.setDoB(temp);
+    cin >> temp;
+    prof.setID(temp);
+    cin >> temp;
+    prof.setName(temp);
+    cin >> x;
+    prof.setSalary(x);
+    cin >> x;
+    prof.setSSN(x);
+
+    cout << prof.getName() << " " << prof.getAddress() << " " << prof.getDegree() << " " << prof.getDoB() << " " << prof.getID() << " " << prof.getSalary() << " " << prof.getSSN();
 
     return 0;
 }
